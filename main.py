d57805c37a020f061e4f8662b2c7d1c02d32f95b
Sebastian Gomez <sebomez05@icloud.com>
2026-03-11 22:33:36 -0500

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import http.server
-import socketserver
-import webbrowser
 
-PORT = 8000
-
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Server running at http://localhost:8000")
-    webbrowser.open("http://localhost:8000/index.html")
-    httpd.serve_forever()
